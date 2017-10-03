8add77dc52bce257d047ccef12bee1de6689ccec
hgb123 <pippi.blockhead@gmail.com>
2017-10-03 11:22:27 +0700

    init app.js and token.js

---
+++ apps/api/app.js
@@ -0,0 +1,96 @@
+var express = require("express");
+var body_parser = require("body-parser");
+var config = require("../../config/config");
+
+/* ===== Express setup ===== */
+var app = express();
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json({
+    verify: function (req, res, buf, encoding) {
+        req.rawBody = buf;
+    }
+}));
+/* ===== End Express setup ===== */
+
+/* ===== Components setup ===== */
+// Datacontext
+var mysql_data_context = require("../../repository/mysql-context")(config.mysql);
+
+// Repository
+
+// Service
+
+// Controller
+
+/* ===== End Components setup  ===== */
+
+require("./routes/authen-routes")(app, authen_controller);
+require("./routes/user-routes")(app, user_controller);
+require("./routes/product-routes")(app, product_controller);
+require("./routes/order-routes")(app, user_controller);
+
+app.use(function (err, req, res, next) {
+    console.error(new Date());
+    console.error(err);
+    if (err.message) {
+        switch (err.message) {
+            case "Spam.":
+                err.code = 9991; return res.status(9991).send(err);
+            case "Product is not existed.":
+                err.code = 9992; return res.status(9992).send(err);
+            case "Code verify is incorrect.":
+                err.code = 9993; return res.status(9993).send(err);
+            case "No data or end of list data.":
+                err.code = 9994; return res.status(9994).send(err);
+            case "User is not validated.":
+                err.code = 9995; return res.status(9995).send(err);
+            case "User is existed.":
+                err.code = 9996; return res.status(9996).send(err);
+            case "Method is invalid.":
+                err.code = 9997; return res.status(9997).send(err);
+            case "Token is invalid.":
+                err.code = 9998; return res.status(9998).send(err);
+            case "Exception error.":
+                err.code = 9999; return res.status(9999).send(err);
+            case "Can not connect to database.":
+                err.code = 1001; return res.status(1001).send(err);
+            case "Parameter is not enough.":
+                err.code = 1002; return res.status(1002).send(err);
+            case "Parameter type is invalid.":
+                err.code = 1003; return res.status(1003).send(err);
+            case "Parameter value is invalid.":
+                err.code = 1004; return res.status(1004).send(err);
+
+            case "File size is too big.":
+                err.code = 1006; return res.status(1006).send(err);
+            case "Upload file failed.":
+                err.code = 1007; return res.status(1007).send(err);
+            case "Maximum number of images.":
+                err.code = 1008; return res.status(1008).send(err);
+            case "Not access.":
+                err.code = 1009; return res.status(1009).send(err);
+            case "Action has been done previously by this user.":
+                err.code = 1010; return res.status(1010).send(err);
+            case "The product has been sold.":
+                err.code = 1011; return res.status(1011).send(err);
+            case "Address does not support shipping.":
+                err.code = 1012; return res.status(1012).send(err);
+            case "Url User\'s is exist.":
+                err.code = 1013; return res.status(1013).send(err);
+            case "Promotional code expired.":
+                err.code = 1014; return res.status(1014).send(err);
+        }
+    } else {
+        err.message = "Unknown error.";
+        err.code = 1005;
+        return res.status(1005).send(err);
+    }
+});
+
+var port = config.port;
+app.listen(port, function () {
+    console.log("Environment: ", process.env.NODE_ENV);
+    console.log("Server is listening on port: ", port);
+});
+
+module.exports = app;
---
+++ apps/api/middlewares/token.js
@@ -0,0 +1,19 @@
+var jwt = require("jsonwebtoken");
+var config = require("../../../config/config");
+
+exports.verify = function(req, res, next) {
+    var token = req.headers["authorization"];    
+    req.token = token;
+    
+    jwt.verify(token, config.authen.secret, function(err, decoded) { 
+        if (err) {
+            return res.status(401).send({ 
+                code: 401, 
+                message: ["Unauthorized"]
+            });
+        } else {
+            req.authen_user = decoded;
+            next();
+        }
+    });
+}
