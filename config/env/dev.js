53b2eb28865fc7d72bbd0405bfb203e21263c878
hgb123 <pippi.blockhead@gmail.com>
2017-10-03 10:19:03 +0700

    init files

---
+++ config/env/dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: 3000,
+    mysql: {
+        username: "enzimer",
+        password: "Geekdev@123456#",
+        database: "moki",
+        host: "mcbooks-satellite.caqfpadax79e.ap-southeast-1.rds.amazonaws.com",
+        dialect: "mysql"
+    },
+    authen: {
+        secret: "it4887#dev"
+    }
+}
---
+++ config/env/local.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: 3000,
+    mysql: {
+        username: "root",
+        password: "Haha123456789",
+        database: "moki",
+        host: "localhost:3306",
+        dialect: "mysql"
+    },
+    authen: {
+        secret: "it4887#local"
+    }
+}
