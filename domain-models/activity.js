9dabe7711b06400fc9af1455a03b33106161b2b8
hgb123 <pippi.blockhead@gmail.com>
2017-11-09 19:50:00 +0700

    add activity model

---
+++ domain-models/activity.js
@@ -0,0 +1,8 @@
+var keymirror = require("keymirror");
+
+module.exports = keymirror({
+    PRODUCT_LIKED: null,
+    PRODUCT_COMMENTED: null,
+    PRODUCT_POSTED: null,
+    CONVERSATION_REQUESTED: null
+});
